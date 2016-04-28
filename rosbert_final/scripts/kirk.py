1494c2ab2c8f3bfa8d03d78723ce32f9e26fb7f2
ChrisBove <bove.3123@gmail.com>
2016-04-27 23:23:49 -0400

    outline of what we need to do

---
+++ rosbert_final/scripts/kirk.py
@@ -0,0 +1,15 @@
+class Kirk:
+	"""Boldly goes where no man has gone before"""
+	def __init__(self):
+		self.i = 0
+
+		# has robot spin around
+		# gets list of edges
+		# runs through and calculates straighline lengths for all of them
+		# filters out the edges which are smaller than the robot
+		# checks if we still have any left - otherwise, notify the makers
+		# chooses the one with the least cost - probably just straightline distance
+			#in the future, we could run Astar on all of them and choose the one with best path
+			# or have a history which picks the biggest one eventually
+		# sends that as a goal to astar, lets robot move there and report it is done the move
+		
